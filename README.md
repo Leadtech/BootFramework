dfbf827e12010b87d0525af7ede72468a120d9d0
Daan Biesterbos <daanbiesterbos@gmail.com>
2016-08-21 20:09:45 +0200

    Update examples + readme

--- README.md
+++ README.md
@@ -163,7 +163,6 @@ class EmployeeService extends AbstractService
         // For demo purposes only:
         // echo $this->getServiceContainer()->get('blaat');
     
-        // This service method returns a raw array
         return [
             ['id' => 1, 'firstName' => 'Jan', 'lastName' => 'Bakker', 'age' => 30],
             ['id' => 2, 'firstName' => 'Ben', 'lastName' => 'Gootmaker', 'age' => 32],
--- examples/microservices/demo1/src/Services/EmployeeService.php
+++ examples/microservices/demo1/src/Services/EmployeeService.php
@@ -12,32 +12,29 @@ use Symfony\Component\HttpFoundation\Response;
 class EmployeeService extends AbstractService
 {
     /**
-     * Returns all employees.
+     * Returns all employees
      *
-     * @param Request $request A request object
+     * @param Request $request     A request object
      *
-     * @return array Arrays or instances of JsonSerializable are automatically encoded as json
+     * @return array               Arrays or instances of JsonSerializable are automatically encoded as json
      */
     public function all(Request $request)
     {
-        // Only for demo purposes:
-        //$service = $this->getServiceContainer()->get('some-service');
+        // For demo purposes only:
+        // echo $this->getServiceContainer()->get('blaat');
 
-        // This service method returns a raw array
         return [
             ['id' => 1, 'firstName' => 'Jan', 'lastName' => 'Bakker', 'age' => 30],
             ['id' => 2, 'firstName' => 'Ben', 'lastName' => 'Gootmaker', 'age' => 32],
-            ['id' => 3, 'firstName' => 'Nico', 'lastName' => 'Fransen', 'age' => 24],
-            ['id' => 4, 'firstName' => 'Jacob', 'lastName' => 'Roos', 'age' => 27],
         ];
     }
 
     /**
-     * Update an employee.
+     * Update an employee
      *
-     * @param Request $request A request object
+     * @param Request $request     A request object
      *
-     * @return string A textual response is outputted as is
+     * @return string              A textual response is outputted as is
      */
     public function update(Request $request)
     {
@@ -47,9 +44,8 @@ class EmployeeService extends AbstractService
     /**
      * This method will delete an employee and send a 201 Accepted on success.
      *
-     * @param Request $request A request object
-     *
-     * @return Response A regular symfony response object
+     * @param Request $request    A request object
+     * @return Response           A regular symfony response object
      */
     public function delete(Request $request)
     {
@@ -59,9 +55,8 @@ class EmployeeService extends AbstractService
     /**
      * This method will add an employee and send a 201 Accepted on success.
      *
-     * @param Request $request A request object
-     *
-     * @return Response A regular symfony response object
+     * @param Request $request    A request object
+     * @return Response           A regular symfony response object
      */
     public function create(Request $request)
     {
